5516339c49bbc871f48f460b55a74474591ab828
Nikhil Mac <nikhilmac@Nikhils-MacBook-Pro.local>
2023-01-01 07:34:24 -0800

    Getting Started

---
+++ Chat Application/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat Application
+//
+//  Created by Nikhil Mac on 01/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
